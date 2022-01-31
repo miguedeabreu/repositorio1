59996b880d4746414793e6a05bdc4466ecf3806d
miguedeabreu <miguedeabreu@gmail.com>
2022-01-31 15:09:40 +0100

    Se agrego la función cuadrado de un número

---
+++ index.js
@@ -0,0 +1,41 @@
+function suma (x,y)
+{
+    let resultado = x+y;
+    return resultado;
+}
+
+console.log(suma(2,5));
+
+function resta (x,y)
+{
+    let resultado = x-y;
+    return resultado;
+}
+
+console.log(resta(10,5));
+
+function producto (x,y)
+{
+    let resultado = x*y;
+    return resultado;
+}
+
+console.log(producto(5,5));
+
+function division (x,y)
+{
+    let resultado = x/y;
+    return resultado
+}
+
+console.log(division(50,10));
+
+function cuadrado (x)
+{
+    let resultado = x**2;
+    return resultado
+}
+
+console.log(cuadrado(5));
+
+module.exports = {suma, resta, producto, division}
